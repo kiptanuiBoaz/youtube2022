5a3fbee38c9155612f1e23cd0987e90f35ab4276
boaz serem <boaserem022@gmail.com>
2022-11-05 23:14:36 +0300

    added file upload funcitonlity

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -2,13 +2,47 @@ import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { collection, getDocs ,deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { async } from "@firebase/util";
 
 const Datatable = () => {
-  const [data, setData] = useState(userRows);
+  const [data, setData] = useState([]);
 
-  const handleDelete = (id) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      let list = [];
+
+      try {
+        const querySnapshot = await getDocs(collection(db, "users"));
+
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          list.push({id:doc.id, ...doc.data()});
+        });
+
+        setData(list);
+        console.log(list);
+
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+    fetchData();
+
+  }, [])
+
+  const handleDelete = async(id) => {
+    try {
+      await deleteDoc(doc(db, "users", id));
     setData(data.filter((item) => item.id !== id));
+
+    } catch (error) {
+      console.log(error);
+      
+    }
   };
 
   const actionColumn = [
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -3,21 +3,58 @@ import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
-import { setDoc, serverTimestamp, doc } from "firebase/firestore";
+import {storageRef ,setDoc, serverTimestamp, doc } from "firebase/firestore";
 import { auth, db, storage } from "../../firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { ref, uploadBytesResumable } from "firebase/storage";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 
 const New = ({ inputs, title }) => {
   const [file, setFile] = useState("");
   const [data, setData] = useState({});
+  const [perc, setPerce] = useState(null);
 
   useEffect(() => {
     const uploadFile = () => {
 
       const name = new Date().getTime() + file.name;
-      console.log(name);
-      // const storageRef = ref(storage, file.name);
+      const storageRef = ref(storage, name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observers:
+      // 1. 'state_changed' observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on('state_changed',
+        (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setPerce(progress);
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+            default:
+              break;
+          }
+        },
+        (error) => {
+          // Handle unsuccessful uploads
+          console.log(error);
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setData((prev) => ({ ...prev, img: downloadURL }))
+          });
+        }
+      );
+
     }
     file && uploadFile();
   }, [file])
@@ -92,7 +129,7 @@ const New = ({ inputs, title }) => {
                   <input id={id} onChange={handleInput} type={type} placeholder={placeholder} />
                 </div>
               ))}
-              <button onClick={handleAdd}>Send</button>
+              <button dissbled={perc !==null &&  perc < 100} onClick={handleAdd}>Send</button>
             </form>
           </div>
         </div>
